e028a09b3ac01cf658dfaec7da656f2e9f8afabc
Sakmongkhon 018 <aucss02@gamil.com>
2022-11-08 11:40:49 +0700

    up

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Control Structure");
+        ifelseStaement if1 = new ifelseStaement();
+//        IfelseStatement if2 = new IfelseStatement();
+
+        if1.testIf();
+        if1.testIfelse();
+        if1.testIfElse2();
+        if1.testIfElseifElse();
+        if1.nestedIfElse();
+    }
+}
